f956cdb882b77d52028fd02fc4e838d546d306fd
MosesMachua <19628623+MosesMachua@users.noreply.github.com>
2022-02-21 18:43:09 -0500

    test

--- Program.cs
+++ Program.cs
@@ -15,10 +15,10 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
 
 var app = builder.Build();
 
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost
-});
+//app.UseForwardedHeaders(new ForwardedHeadersOptions
+//{
+//    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost
+//});
 
 
 // Configure the HTTP request pipeline.
